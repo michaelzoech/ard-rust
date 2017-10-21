0f659710ee844a223608dd851735b630b65ce1fe
Michael Zoech <michi.zoech@gmail.com>
2017-10-20 21:01:17 -0700

    Add current raytracer as 'metal_spheres' sample

---
+++ examples/metal_spheres.rs
@@ -0,0 +1,60 @@
+extern crate ard;
+
+use std::rc::Rc;
+use std::time::Instant;
+
+use ard::camera::*;
+use ard::color::*;
+use ard::material::*;
+use ard::math::*;
+use ard::sampler::*;
+use ard::shapes::*;
+use ard::trace::*;
+
+fn main() {
+
+    let trace_config = TraceConfig {
+        image_width: 640,
+        image_height: 480,
+        pixel_size: 0.01,
+        pixel_sampler: Sampler::regular_sampler(8, 0.0),
+        max_trace_depth: 10,
+    };
+
+    let mut tracer = Tracer::new(&trace_config);
+
+    let camera = PinholeCamera::new(&Vector3::new(0.0, 2.0, 4.5), &Vector3::new(0.0, 1.2, 0.0), &Vector3::new(0.0, 1.0, 0.0), 4.0);
+
+    let objects: Vec<Box<Hitable>> = vec![
+        Box::new(Sphere {
+            center: Vector3::new(2.0, 1.0, 0.0),
+            radius: 1.0,
+            material: Rc::new(Metal::new(&Sampler::jittered_sampler(8, 0.0), &Color { r: 0.8, g: 0.8, b: 0.8, a: 1.0 })),
+        }),
+        Box::new(Sphere {
+            center: Vector3::new(-2.0, 1.0, 0.0),
+            radius: 1.0,
+            material: Rc::new(Metal::new(&Sampler::jittered_sampler(8, 0.0), &Color { r: 0.8, g: 0.6, b: 0.2, a: 1.0 })),
+        }),
+        Box::new(Sphere {
+            center: Vector3::new(0.0, 1.0, 0.0),
+            radius: 1.0,
+            material: Rc::new(Lambertian::new(&Sampler::jittered_sampler(8, 1.0), &Color { r: 1.0, g: 0.3, b: 1.0, a: 1.0 })),
+        }),
+        Box::new(Sphere {
+            center: Vector3::new(0.0, -100.0, 0.0),
+            radius: 100.0,
+            material: Rc::new(Lambertian::new(&Sampler::jittered_sampler(8, 100.0), &Color { r: 0.8, g: 1.0, b: 0.0, a: 1.0 })),
+        }),
+    ];
+
+    let start_time = Instant::now();
+
+    tracer.trace(&camera, &objects);
+
+    let elapsed = start_time.elapsed().as_secs();
+
+    println!("Image rendered in {0} seconds", elapsed);
+
+    tracer.write_to_file("metal_spheres.bmp").expect("Cannot write bitmap");
+}
